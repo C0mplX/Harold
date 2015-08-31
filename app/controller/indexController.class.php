2ffc6c947e81344df0dd9e85ef5392b877a770ca
Ole Martin <olemartin.skaug@gmail.com>
2015-08-31 23:23:40 +0200

    Added indexController

--- app/controller/indexController.class.php
+++ app/controller/indexController.class.php
@@ -7,11 +7,18 @@ require_once( __DIR__ . '/../../core/init.php' );
 * @since 1.0
 *
 */
-class IndexController extends View{
+class IndexController extends View {
 	
 	public function __construct() {
 		
+		#Set the title text
+
+		#Require the header view.
+		$this->renderView( 'header', 'template', array( 'title' => 'Welcome to Harold' ) );
+		#render main content view
 		$this->renderView( 'index', '' );
+		#render the footer view
+		$this->renderView( 'footer', 'template' );
 
 	}
 }
--- core/classes/view.class.php
+++ core/classes/view.class.php
@@ -9,7 +9,14 @@ require_once( __DIR__ . '/../init.php' );
 */
 class View  {
 	
-	public function renderView( $view, $folder = "" ) {
+	public function renderView( $view, $folder = "", $data = "" ) {
+
+		/**
+		* Check if there is any data transferd to the class
+		*/
+		if( $data != "" ) {
+			$data = $data;
+		}
 
 		if( $folder != "" ) {
 			require_once( ROOT . DS . 'app' . DS . 'view' . DS . $folder . DS . strtolower( $view ) . '.view.php' );	

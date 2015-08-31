e1f62ff5f7dac61db511b5aafeb2419fbd70fb17
Ole Martin <olemartin.skaug@gmail.com>
2015-08-31 22:17:56 +0200

    Startup

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* 
+* Defines a set of rules to use through out the application 
+* 
+*/
+define( 'ROOT', __DIR__ ); //Sets the root directiry
+define( 'DS', DIRECTORY_SEPARATOR ); //Sets the directiory separator
+define( 'DEVELOPER_MODE', true ) //Boolean sets the developer environemnt
+define( 'SITE_URL', '/Harold/' ) //Set this to the absoulte url, like www.example.com
---
+++ core/init.php
@@ -0,0 +1,10 @@
+<?php
+/**
+*
+* Main init file, this contains the autoloader for the classes
+* @author Ole Martin Skaug 
+* @version 1.0
+* @since 1.0
+* 
+*/
+session_start();
